13b3d2fa25d16639619333e5907f33d18a586328
jyo-u <62988778+jyo-u@users.noreply.github.com>
2020-10-19 22:18:00 +0900

    Revert "Revert "cubeunity""
    This reverts commit e3f2796f9d988f217fcefdcf67f6b9fd9c4043d1.

--- toio-sdk-unity/Assets/toio-sdk/Scripts/Cube/CoreCube/Real/Versions/CubeReal_ver2_2_0.cs
+++ toio-sdk-unity/Assets/toio-sdk/Scripts/Cube/CoreCube/Real/Versions/CubeReal_ver2_2_0.cs
@@ -2,37 +2,31 @@ using System.Collections.Generic;
 
 namespace toio
 {
-    public class CubeReal_ver2_1_0 : CubeReal_ver2_0_0
+    public class CubeReal_ver2_2_0 : CubeReal_ver2_1_0
     {
         //_/_/_/_/_/_/_/_/_/_/_/_/_/
         //      内部変数
         //_/_/_/_/_/_/_/_/_/_/_/_/_/
 
-        protected CallbackProvider _doubleTapCallback = new CallbackProvider();
-        protected CallbackProvider _poseCallback = new CallbackProvider();
+        protected CallbackProvider _shakeCallback = new CallbackProvider();
 
         //_/_/_/_/_/_/_/_/_/_/_/_/_/
         //      外部変数
         //_/_/_/_/_/_/_/_/_/_/_/_/_/
+        public override bool isShake { get; protected set; }
+        public override string version { get { return "2.2.0"; } }
 
-        public override bool isDoubleTap { get; protected set; }
-        public override PoseType pose { get; protected set; }
-        public override string version { get { return "2.1.0"; } }
-        public override int maxSpd { get { return 115; } }
+        // シェイクコールバック
+        public override CallbackProvider shakeCallback { get { return this._shakeCallback; } }
 
-        // ダブルタップコールバック
-        public override CallbackProvider doubleTapCallback { get { return this._doubleTapCallback; } }
-        // 姿勢コールバック
-        public override CallbackProvider poseCallback { get { return this._poseCallback; } }
-
-        public CubeReal_ver2_1_0(BLEPeripheralInterface peripheral, Dictionary<string, BLECharacteristicInterface> characteristicTable)
+        public CubeReal_ver2_2_0(BLEPeripheralInterface peripheral, Dictionary<string, BLECharacteristicInterface> characteristicTable)
         : base(peripheral, characteristicTable)
         {
         }
 
         protected override void Recv_sensor(byte[] data)
         {
-            // https://toio.github.io/toio-spec/docs/2.1.0/ble_sensor
+            // https://toio.github.io/toio-spec/docs/ble_sensor
             int type = data[0];
             if (1 == type)
             {
@@ -40,7 +34,7 @@ namespace toio
                 var _isCollisionDetected = data[2] == 1 ? true : false;
                 var _isDoubleTap = data[3] == 1 ? true : false;
                 PoseType _pose = (PoseType)data[4];
-
+                var _isShake = data[5] == 1 ? true : false;
 
                 if (_isSloped != this.isSloped)
                 {
@@ -64,7 +58,12 @@ namespace toio
                 {
                     this.pose = _pose;
                     this.poseCallback.Notify(this);
+                }
 
+                if (_isShake != this.isShake)
+                {
+                    this.isShake = _isShake;
+                    this.shakeCallback.Notify(this);
                 }
             }
         }
