bd3cc657b087eaf2ef126774018b6c73bebd16dc
morika-shomma <49185818+morika-shomma@users.noreply.github.com>
2020-12-17 17:51:14 +0900

    update(TestBuildScript):add editor play mode

--- toio-sdk-unity/Assets/toio-sdk/Tests/CoffeeMode/Core/Editor/TestBuildScript.cs
+++ toio-sdk-unity/Assets/toio-sdk/Tests/CoffeeMode/Core/Editor/TestBuildScript.cs
@@ -17,55 +17,98 @@ namespace toio.Tests
 {
     public class TestBuildScript
     {
-        private const string KEY_COMPILE_STATUS = "KEY_COMPILE_STATUS";
+        //
+        private const string KEY_TEST_STATUS = "KEY_COMPILE_STATUS";
+        private const string VAL_TEST_STATUS_CREATE_SCRIPT = "VAL_TEST_STATUS_CREATESCRIPT";
+        private const string VAL_TEST_STATUS_CREATE_SCENE = "VAL_TEST_STATUS_CREATESCENE";
+        private const string VAL_TEST_STATUS_RUN_EDITOR = "VAL_TEST_STATUS_RUN_EDITOR";
+        //
         private const string KEY_GENSCRIPT_PATH = "KEY_GENSCRIPT_PATH";
+        //
         private const string KEY_GENSCENE_PATH = "KEY_GENSCENE_PATH";
-        private const string VAL_BEFORE_COMPILE = "VAL_BEFORE_COMPILE";
-        private const string VAL_AFTER_COMPILE = "VAL_AFTER_COMPILE";
+        //
+        private const string KEY_TESTENV = "KEY_TEST_ENV";
+        private const string VAL_TESTENV_EDITOR = "VAL_TESTENV_EDITOR";
+        private const string VAL_TESTENV_APP = "VAL_TESTENV_APP";
 
-        private const string OriginalTestScenePath = "Assets/toio-sdk/Tests/CoffeeMode/Core/Scene/TestScene.unity";
-        private const string DestDir = "Assets/";
-
-        [MenuItem ("toio/run test")]
-        public static void MenuItem()
+        static TestBuildScript()
         {
-            EditorPrefs.SetString(KEY_COMPILE_STATUS, VAL_BEFORE_COMPILE);
+            EditorApplication.playModeStateChanged += (stateChange) =>
+            {
+                if (PlayModeStateChange.ExitingPlayMode == stateChange) { Reset(); }
+            };
+        }
+
+        [MenuItem ("toio/run test/Editor")]
+        public static void MenuItem_Editor()
+        {
+            EditorPrefs.SetString(KEY_TEST_STATUS, VAL_TEST_STATUS_CREATE_SCRIPT);
+            EditorPrefs.SetString(KEY_TESTENV, VAL_TESTENV_EDITOR);
+            BuildTest();
+        }
+        [MenuItem ("toio/run test/App")]
+        public static void MenuItem_App()
+        {
+            EditorPrefs.SetString(KEY_TEST_STATUS, VAL_TEST_STATUS_CREATE_SCRIPT);
+            EditorPrefs.SetString(KEY_TESTENV, VAL_TESTENV_APP);
             BuildTest();
         }
 
         [DidReloadScripts]
-        // ref https://answers.unity.com/questions/1007004/generate-script-in-editor-script-and-gettype-retur.html
-        public static async void BuildTest ()
+        public static async void BuildTest()
         {
-            if (!EditorPrefs.HasKey(KEY_COMPILE_STATUS)) { return; }
+            Debug.Log("---on reload---");
+            // 無効の場合はreturn
+            if (!EditorPrefs.HasKey(KEY_TEST_STATUS)) { return; }
 
-            var status = EditorPrefs.GetString(KEY_COMPILE_STATUS);
-
+            // 基本的にはコンパイル発生毎に分岐する
+            // ref https://answers.unity.com/questions/1007004/generate-script-in-editor-script-and-gettype-retur.html
+            var status = EditorPrefs.GetString(KEY_TEST_STATUS);
             try
             {
-                if (VAL_BEFORE_COMPILE == status)
+                if (VAL_TEST_STATUS_CREATE_SCRIPT == status)
                 {
-                    // 1. スクリプト生成
-                    var scriptPath = CreateScript();
-                    EditorPrefs.SetString(KEY_COMPILE_STATUS, VAL_AFTER_COMPILE);
+                    Debug.Log("VAL_TEST_STATUS_CREATE_SCRIPT start");
+                    // 1.スクリプト生成
+                    var scriptPath = CreateScript("Assets/");
+                    EditorPrefs.SetString(KEY_TEST_STATUS, VAL_TEST_STATUS_CREATE_SCENE);
                     EditorPrefs.SetString(KEY_GENSCRIPT_PATH, scriptPath);
-                    // スクリプト生成処理完了後に明示的にアセット読み込み
+                    // 2.スクリプト生成処理完了後に明示的にアセット読み込み
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
+                    Debug.Log("VAL_TEST_STATUS_CREATE_SCRIPT end");
                 }
-                else if (VAL_AFTER_COMPILE == status)
+                else if (VAL_TEST_STATUS_CREATE_SCENE == status)
                 {
+                    Debug.Log("VAL_TEST_STATUS_CREATE_SCENE start");
                     // 1.アセンブリロードコールバック時ではシーンが開けないため、Yieldを利用してコンテクストを変更
                     await Task.Yield();
                     // 2.テスト用シーンを生成
                     var scriptPath = EditorPrefs.GetString(KEY_GENSCRIPT_PATH);
-                    var scenePath = CreateTestScene(scriptPath);
+                    var scenePath = CreateTestScene(scriptPath, "Assets/");
                     EditorPrefs.SetString(KEY_GENSCENE_PATH, scenePath);
-                    // 3.ビルド
-                    Build(scenePath);
-
-                    //
-                    Reset();
+                    // 3.テストを実行
+                    var env = EditorPrefs.GetString(KEY_TESTENV);
+                    if (VAL_TESTENV_EDITOR == env)
+                    {
+                        EditorApplication.EnterPlaymode();
+                        EditorPrefs.SetString(KEY_TEST_STATUS, VAL_TEST_STATUS_RUN_EDITOR);
+                        // エディターの場合はプレイモード終了時にリセット
+                    }
+                    else if (VAL_TESTENV_APP == env)
+                    {
+                        BuildTestApp(scenePath, env);
+                        Reset();
+                    }
+                    Debug.Log("VAL_TEST_STATUS_CREATE_SCENE end");
+                }
+                else if (VAL_TEST_STATUS_RUN_EDITOR == status)
+                {
+                    // 何もしない
+                    /* note
+                       Unity2019.3以前 ではプレイモード遷移事にコンパイルが入るため、条件分岐しておく
+                       Unity2019.3以後 ではプレイモード遷移時にコンパイルが入らない場合があるため、プレイモード終了時のリセット処理は playModeStateChanged でイベント実行する
+                    */
                 }
             }
             catch(Exception e)
@@ -75,21 +118,10 @@ namespace toio.Tests
             }
         }
 
-        static void Reset()
+        static string CreateScript(string targetDir)
         {
-            Debug.Log("Reset");
-            if (EditorPrefs.HasKey(KEY_GENSCRIPT_PATH)) { AssetDatabase.DeleteAsset(EditorPrefs.GetString(KEY_GENSCRIPT_PATH)); }
-            if (EditorPrefs.HasKey(KEY_GENSCENE_PATH)) { AssetDatabase.DeleteAsset(EditorPrefs.GetString(KEY_GENSCENE_PATH)); }
-
-            EditorPrefs.DeleteKey(KEY_COMPILE_STATUS);
-            EditorPrefs.DeleteKey(KEY_GENSCRIPT_PATH);
-            EditorPrefs.DeleteKey(KEY_GENSCENE_PATH);
-        }
-
-        static string CreateScript()
-        {
-            // ---- 1.テストスクリプトを生成 ----  //
-            string testScriptName = "CLASS" + Guid.NewGuid().ToString("N").Substring(0, 10);
+            // 0.変数定義
+            string testScriptName = "_" + Guid.NewGuid().ToString("N").Substring(0, 10);
             const string CodeTemplate = @"
                 using UnityEngine;
                 using toio.Tests;
@@ -105,7 +137,7 @@ namespace toio.Tests
             // 1.テスト関数を取得
             var testMethods = GetTestMethods(typeof(CoffeeTestCase), typeof(CoffeeTest));
 
-            // 2.書き込み範囲(##CODE##)に書き込む文字列を生成
+            // 2.書き込み範囲(%%CODE%%)に書き込む文字列を生成
             var classHash = new HashSet<Type>();
             var sb = new StringBuilder();
             foreach(var m in testMethods)
@@ -125,13 +157,27 @@ namespace toio.Tests
             var code = CodeTemplate.Replace("%%CLASS%%", testScriptName).Replace("%%CODE%%", sb.ToString());
 
             // 4.スクリプトを生成
-            return GenerateScript(Path.Combine(DestDir, testScriptName + ".cs"), code);
+            return GenerateScript(Path.Combine(targetDir, testScriptName + ".cs"), code);
         }
 
-        static string CreateTestScene(string testScriptPath)
+        static void Reset()
         {
+            Debug.Log("Reset");
+            if (EditorPrefs.HasKey(KEY_GENSCRIPT_PATH)) { AssetDatabase.DeleteAsset(EditorPrefs.GetString(KEY_GENSCRIPT_PATH)); }
+            if (EditorPrefs.HasKey(KEY_GENSCENE_PATH)) { AssetDatabase.DeleteAsset(EditorPrefs.GetString(KEY_GENSCENE_PATH)); }
+
+            EditorPrefs.DeleteKey(KEY_TEST_STATUS);
+            EditorPrefs.DeleteKey(KEY_GENSCRIPT_PATH);
+            EditorPrefs.DeleteKey(KEY_GENSCENE_PATH);
+            EditorPrefs.DeleteKey(KEY_TESTENV);
+        }
+
+        static string CreateTestScene(string testScriptPath, string targetDir)
+        {
+            // 0.変数定義
             string randomFileName = "_" + Guid.NewGuid().ToString("N").Substring(0, 10);
-            var scenePath = Path.Combine(DestDir, randomFileName + ".unity");
+            const string OriginalTestScenePath = "Assets/toio-sdk/Tests/CoffeeMode/Core/Scene/TestScene.unity";
+            var scenePath = Path.Combine(targetDir, randomFileName + ".unity");
 
             // 1.一時シーンを生成
             if (!CopyScene(OriginalTestScenePath, scenePath)) { Debug.LogError("一時テストシーンの生成に失敗しました"); }
@@ -147,58 +193,21 @@ namespace toio.Tests
             return scenePath;
         }
 
-        static void Build(string scenePath)
+        static void BuildTestApp(string scenePath, string env)
         {
-            var scenes = new[] { scenePath };
-
-            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.locationPathName = "iOSBuild";
-            buildPlayerOptions.target = EditorUserBuildSettings.activeBuildTarget;
-            buildPlayerOptions.options = BuildOptions.Development;
-
-            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-            BuildSummary summary = report.summary;
-
-            if (summary.result == BuildResult.Succeeded)
+            if (Application.platform == RuntimePlatform.IPhonePlayer)
             {
-                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+                Build(scenePath, "iOSBuild");
             }
-            else if (summary.result == BuildResult.Failed)
+            else if (Application.platform == RuntimePlatform.WebGLPlayer)
             {
-                Debug.LogError("Build failed");
+                Build(scenePath, "WebGLBuild");
+            }
+            else
+            {
+
             }
         }
-
-        // out パスが重複していた場合は自動で数字を付与してファイル生成する
-        // ref https://www.hanachiru-blog.com/entry/2019/12/20/221633
-        static string GenerateScript(string filepath, string code)
-        {
-            var assetPath = AssetDatabase.GenerateUniqueAssetPath(filepath);
-            File.WriteAllText(assetPath, code);
-            // スクリプトの場合はコンパイルが発生してエディタスクリプトが停止するのでコメントアウト
-            //AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
-            //AssetDatabase.SaveAssets();
-            //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
-            return assetPath;
-        }
-
-        static UnityEngine.SceneManagement.Scene GenerateScene(string filepath)
-        {
-            var assetPath = AssetDatabase.GenerateUniqueAssetPath(filepath);
-            var scene = EditorSceneManager.CreateScene(assetPath);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
-            return scene;
-        }
-
-        static bool CopyScene(string filepath, string newfilepath)
-        {
-            var ret = AssetDatabase.CopyAsset(filepath, newfilepath);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
-            return ret;
-        }
-
 
         static List<MethodInfo> GetTestMethods(Type classType, Type attributeType)
         {
@@ -221,21 +230,62 @@ namespace toio.Tests
             return methods;
         }
 
+        //_/_/_/_/_/_/_/_/_/_/_/_/_/
+        //      便利関数
+        //_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+        static void Build(string scenePath, string appPath)
+        {
+            var scenes = new[] { scenePath };
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.locationPathName = appPath;
+            buildPlayerOptions.target = EditorUserBuildSettings.activeBuildTarget;
+            buildPlayerOptions.options = BuildOptions.Development;
+
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+            else if (summary.result == BuildResult.Failed)
+            {
+                Debug.LogError("Build failed");
+            }
+        }
+
+        // out パスが重複していた場合は自動で数字を付与してファイル生成する
+        // ref https://www.hanachiru-blog.com/entry/2019/12/20/221633
+        static string GenerateScript(string filepath, string code)
+        {
+            var assetPath = AssetDatabase.GenerateUniqueAssetPath(filepath);
+            File.WriteAllText(assetPath, code);
+            // note スクリプトの場合はコンパイルが発生してエディタスクリプトが停止するためSaveAssets()やRefresh()は勝手に実行しない
+            return assetPath;
+        }
+
+        static UnityEngine.SceneManagement.Scene GenerateScene(string filepath)
+        {
+            var assetPath = AssetDatabase.GenerateUniqueAssetPath(filepath);
+            var scene = EditorSceneManager.CreateScene(assetPath);
+            //AssetDatabase.SaveAssets();
+            //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
+            return scene;
+        }
+
+        static bool CopyScene(string filepath, string newfilepath)
+        {
+            var ret = AssetDatabase.CopyAsset(filepath, newfilepath);
+            //AssetDatabase.SaveAssets();
+            //AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
+            return ret;
+        }
+
         // ref http://bochituku.jugem.jp/?eid=4
         static Type GetTypeByClassName(string className)
         {
-            /*
-            var assembly = Assembly.Load("Assembly-CSharp");
-
-            foreach(Type type in assembly.GetTypes())
-            {
-                if(type.Name == className)
-                {
-                    return type;
-                }
-            }
-            */
-
             foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 foreach(Type type in assembly.GetTypes())
