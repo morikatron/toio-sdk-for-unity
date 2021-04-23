70deaeafe3031fa34008970f5a4037211c4b43a5
jyo-u <62988778+jyo-u@users.noreply.github.com>
2021-04-23 15:51:22 +0900

    UPDATE

--- toio-sdk-unity/Assets/toio-sdk/Tutorials/2.Advanced-CubeHandle/0.BasicScene/HandleBasic.cs
+++ toio-sdk-unity/Assets/toio-sdk/Tutorials/2.Advanced-CubeHandle/0.BasicScene/HandleBasic.cs
@@ -18,19 +18,36 @@ namespace toio.tutorial
         async void Start()
         {
             cubeManager = new CubeManager();
-            await cubeManager.MultiConnect(1);
         }
 
-        public void bu1()
+
+        public void Disconnect1()
         {
-            Debug.Log(126666666666663);
+            Debug.Log("Disconnect1_start");
             var cube = cubeManager.cubes[0];
             cubeManager.Disconnect(cube);
-            
+            Debug.Log("Disconnect1_over");
         }
-        public async void bu2()
+        public void Disconnect2()
+        {
+            Debug.Log("Disconnect2_start");
+            var cube = cubeManager.cubes[1];
+            cubeManager.Disconnect(cube);
+            Debug.Log("Disconnect2_over");
+        }
+        public async void SingleCon()
         {
             var cube = await cubeManager.SingleConnect();
+        }
+
+        public async void Multi2Con()
+        {
+            var cubes = await cubeManager.MultiConnect(2);
+        }
+
+        public async void Multi1Con()
+        {
+            var cube = await cubeManager.MultiConnect(1);
         }         
     }
 
