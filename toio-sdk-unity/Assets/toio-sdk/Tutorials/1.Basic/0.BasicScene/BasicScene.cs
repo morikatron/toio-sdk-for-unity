44af5794374eb60104d5754e84ffb23bdb927c01
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2021-07-01 18:06:56 +0900

    Update BasicScene.cs

--- toio-sdk-unity/Assets/toio-sdk/Tutorials/1.Basic/0.BasicScene/BasicScene.cs
+++ toio-sdk-unity/Assets/toio-sdk/Tutorials/1.Basic/0.BasicScene/BasicScene.cs
@@ -7,7 +7,9 @@ namespace toio.tutorial
     {
         float intervalTime = 0.05f;
         float elapsedTime = 0;
+        float connectTime = 0;
         Cube cube;
+        CubeConnecter connecter;
 
         // 非同期初期化
         // C#標準機能であるasync/awaitキーワードを使用する事で、検索・接続それぞれで終了待ちする
@@ -18,7 +20,9 @@ namespace toio.tutorial
             // Bluetoothデバイスを検索
             var peripheral = await new NearestScanner().Scan();
             // デバイスへ接続してCube変数を生成
-            cube = await new CubeConnecter().Connect(peripheral);
+            connecter = new CubeConnecter();
+            cube = await connecter.Connect(peripheral);
+            connectTime = Time.time;
         }
 
         void Update()
@@ -28,12 +32,27 @@ namespace toio.tutorial
             // 経過時間を計測
             elapsedTime += Time.deltaTime;
 
-            // 前回の命令から50ミリ秒以上経過した場合
-            if (intervalTime < elapsedTime)
+            // 5秒間隔で、切断と再接続を繰り返す （リアルキューブのみに有効）
+            if (Time.time > connectTime + 5f)
             {
-                elapsedTime = 0.0f;
-                // 左モーター速度:50, 右モーター速度:-50, 制御時間:200ミリ秒
-                cube.Move(50, -50, 200);
+                Debug.LogWarning("!! DisConnect !!");
+                // 接続済みの場合は、切断
+                if (cube.isConnected) connecter.Disconnect(cube);
+                // 切断された場合は、再接続
+                // else connecter.ReConnect(cube);
+                connectTime = Time.time;
+            }
+
+            // 接続された場合は動く
+            if (cube.isConnected)
+            {
+                // 前回の命令から50ミリ秒以上経過した場合
+                if (intervalTime < elapsedTime)
+                {
+                    elapsedTime = 0.0f;
+                    // 左モーター速度:50, 右モーター速度:-50, 制御時間:200ミリ秒
+                    cube.Move(50, -50, 200);
+                }
             }
         }
     }
