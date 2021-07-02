c627b56b9042c5123b12efd4988bc2441a6915f8
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2021-07-02 16:09:26 +0900

    Add Tutorial

---
+++ toio-sdk-unity/Assets/toio-sdk/Tutorials/1.Basic/7.CubeManager/CubeManagerScene_Reconnect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace toio.tutorial
+{
+    public class CubeManagerScene_Reconnect : MonoBehaviour
+    {
+        CubeManager cubeManager;
+        Cube cube;
+
+        async void Start()
+        {
+            // CubeManagerからモジュールを間接利用した場合:
+            cubeManager = new CubeManager();
+            cube = await cubeManager.SingleConnect();
+
+            // 切断・再接続のループを開始
+            if (cube != null) StartCoroutine(LoopConnection());
+        }
+
+        IEnumerator LoopConnection()
+        {
+            yield return new WaitForSeconds(3);
+
+            // 切断 （CubeManager 利用した場合）
+            cubeManager.DisconnectAll();    // ALT: cubeManager.Disconnect(cube);
+            yield return new WaitUntil(() => !cube.isConnected);
+            yield return new WaitForSeconds(3);
+
+            // 再接続 （CubeManager 利用した場合）
+            cubeManager.ReConnectAll();     // ALT: cubeManager.ReConnect(cube);
+            yield return new WaitUntil(() => cube.isConnected);
+
+            StartCoroutine(LoopConnection());
+        }
+
+        void Update()
+        {
+            // CubeManagerからモジュールを間接利用した場合:
+            if (cubeManager.IsControllable(cube))
+            {
+                cube.Move(50, -50, 200);
+            }
+        }
+    }
+}
