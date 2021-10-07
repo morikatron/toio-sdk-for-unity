5411de865ab51819133830cfeab2a5342f2919b5
morika-shomma <49185818+morika-shomma@users.noreply.github.com>
2021-10-07 12:48:41 +0900

    add data event callback to BLECharacteristic

--- toio-sdk-unity/Assets/toio-sdk/Scripts/BLE/mobile/BLEMobileCharacteristic.cs
+++ toio-sdk-unity/Assets/toio-sdk/Scripts/BLE/mobile/BLEMobileCharacteristic.cs
@@ -7,12 +7,17 @@ namespace toio
         public string deviceAddress { get; private set; }
         public string serviceUUID { get; private set; }
         public string characteristicUUID { get; private set; }
+        public TCallbackProvider<string, byte[]> readDataCallback { get; private set; }
+        public TCallbackProvider<string, byte[], bool> writeDataCallback { get; private set; }
+
 
         public BLEMobileCharacteristic(string deviceAddress, string serviceUUID, string characteristicUUID)
         {
             this.deviceAddress = deviceAddress;
             this.serviceUUID = serviceUUID.ToUpper();
             this.characteristicUUID = characteristicUUID.ToUpper();
+            this.readDataCallback = new TCallbackProvider<string, byte[]>();
+            this.writeDataCallback = new TCallbackProvider<string, byte[], bool>();
         }
 
         public void ReadValue(Action<string, byte[]> action)
@@ -20,6 +25,7 @@ namespace toio
             Ble.ReadCharacteristic(this.deviceAddress, this.serviceUUID, this.characteristicUUID, (address, characteristicUUID, bytes) =>
             {
                 action(characteristicUUID, bytes);
+                this.readDataCallback.Notify(characteristicUUID, bytes);
             });
         }
         public void WriteValue(byte[] data, bool withResponse)
@@ -31,6 +37,7 @@ namespace toio
             Ble.SubscribeCharacteristic(this.deviceAddress, this.serviceUUID, this.characteristicUUID, (address, characteristicUUID, bytes) =>
             {
                 action(bytes);
+                this.readDataCallback.Notify(characteristicUUID, bytes);
             });
         }
         public void StopNotifications()
