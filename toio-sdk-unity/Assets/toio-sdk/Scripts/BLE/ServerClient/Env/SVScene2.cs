babe7d108d859389ba7aa3a99ca79e420b658fc7
morika-shomma <49185818+morika-shomma@users.noreply.github.com>
2021-10-11 14:38:53 +0900

    Update SVScene2.cs

--- toio-sdk-unity/Assets/toio-sdk/Scripts/BLE/ServerClient/Env/SVScene2.cs
+++ toio-sdk-unity/Assets/toio-sdk/Scripts/BLE/ServerClient/Env/SVScene2.cs
@@ -8,14 +8,12 @@ public class SVScene2 : MonoBehaviour
 
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
         BLEService.Instance.SetImplement(new BLENetService("192.168.0.9", 50006));
 
         // CubeManagerからモジュールを間接利用した場合:
         cubeManager = new CubeManager(ConnectType.Real);
         // 任意のタイミングで非同期接続
-        cubeManager.MultiConnectAsync(cubeNum:20, coroutineObject:this, OnConnected);
+        cubeManager.MultiConnectAsync(cubeNum:20, coroutineObject:this);
     }
 
     void Update()
@@ -25,19 +23,6 @@ public class SVScene2 : MonoBehaviour
         {
             cube.Move(50, -50, 200);
             Debug.LogFormat("x: {0}, y: {1}", cube.pos.x, cube.pos.y);
-        }
-    }
-
-    void OnConnected(Cube cube, CONNECTION_STATUS status)
-    {
-        Debug.Log(cube.localName);
-        if (status.IsNewConnected)
-        {
-            Debug.Log("new-connected!!");
-        }
-        else if (status.IsReConnected)
-        {
-            Debug.Log("re-connected!!");
         }
     }
 #endif
