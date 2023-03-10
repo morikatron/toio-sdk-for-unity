079399edff8278b1657fed5420b5ce2d97992e86
neon-izm <jj.y0sh1da@gmail.com>
2023-03-10 20:12:40 +0900

    permission request should devide with api level

--- toio-sdk-unity/Assets/ble-plugin-unity/Plugins/Android/Runtime/BlePermissionRequest.cs
+++ toio-sdk-unity/Assets/ble-plugin-unity/Plugins/Android/Runtime/BlePermissionRequest.cs
@@ -3,10 +3,12 @@
 #define UNITY_ANDROID_RUNTIME
 #endif
 
-
 #if UNITY_ANDROID_RUNTIME
+using System.Collections.Generic;
 using System.Collections;
 using System;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Android;
 
@@ -14,6 +16,13 @@ namespace toio.Android
 {
     public class BlePermissionRequest
     {
+        const string androidFineLocationPermission = "android.permission.ACCESS_FINE_LOCATION";
+        const string androidCourceLocationPermission = "android.permission.ACCESS_COARSE_LOCATION";
+        const string androidBluetoothPermission = "android.permission.BLUETOOTH";
+        const string androidBluetoothAdminPermission = "android.permission.BLUETOOTH_ADMIN";
+        const string androidBluetoothAdvertisePermission = "android.permission.BLUETOOTH_ADVERTISE";
+        const string androidBluetoothScanPermission = "android.permission.BLUETOOTH_SCAN";
+        const string androidBluetoothConnectPermission = "android.permission.BLUETOOTH_CONNECT";
         Action initializedAction;
         Action<string> errorAction;
 
@@ -26,23 +35,55 @@ namespace toio.Android
 
         public IEnumerator Request()
         {
-            string permission = Permission.FineLocation;
-            if (!Permission.HasUserAuthorizedPermission(permission))
+            
+            var androidOsVersion = GetSdkInt();
+            var willAskPermissions = new List<string>();
+            if (androidOsVersion >= 31)
+            {
+                willAskPermissions.Add(androidBluetoothAdvertisePermission);
+                willAskPermissions.Add(androidBluetoothScanPermission);
+                willAskPermissions.Add(androidBluetoothConnectPermission);
+            }
+            else
+            {
+                willAskPermissions.Add(androidBluetoothPermission);
+                willAskPermissions.Add(androidBluetoothAdminPermission);
+            }
+
+            willAskPermissions.Add(androidFineLocationPermission);
+
+            foreach (var permission in willAskPermissions.Where(permission =>
+                         !Permission.HasUserAuthorizedPermission(permission)))
             {
                 Permission.RequestUserPermission(permission);
             }
+
             yield return null;
-            if (Permission.HasUserAuthorizedPermission(permission))
+            var failedList = willAskPermissions.Where(permission => Permission.HasUserAuthorizedPermission(permission) == false).ToList();
+
+            if (failedList.Count == 0)
             {
                 this.initializedAction();
             }
             else
             {
-                this.errorAction("No Permission:" + permission);
+                this.errorAction("No Permission:" + string.Join(",", failedList));
             }
         }
 
+        public static int GetSdkInt()
+        {
+#if UNITY_ANDROID
+            if (Application.isEditor) return -1;
 
+            using (var version = new AndroidJavaClass("android.os.Build$VERSION"))
+            {
+                return version.GetStatic<int>("SDK_INT");
+            }
+#else
+            return -1;
+#endif
+        }
     }
 }
 #endif
