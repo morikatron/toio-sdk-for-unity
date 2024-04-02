dd0e01e6cba42e552e9559a2975c8ba3cdc6a128
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2024-04-02 17:35:55 +0900

    Update Sample_ConnectName: Keep connecting cubes

--- toio-sdk-unity/Assets/toio-sdk/Samples/Sample_ConnectName/Sample_ConnectName.cs
+++ toio-sdk-unity/Assets/toio-sdk/Samples/Sample_ConnectName/Sample_ConnectName.cs
@@ -16,9 +16,15 @@ public class Sample_ConnectName : MonoBehaviour
     private CubeScanner scanner;
     private CubeConnecter connecter;
     private Dictionary<string, GameObject> cubeItems = new Dictionary<string, GameObject>();
-    private List<string> connectedAddrs = new List<string>();
-    private List<BLEPeripheralInterface> connectedPeripherals = new List<BLEPeripheralInterface>();
-    private List<BLEPeripheralInterface> scannedPeripherals = new List<BLEPeripheralInterface>();
+
+    private enum BLEStatus
+    {
+        Disconnected,
+        Connecting,
+        Connected,
+    }
+    private Dictionary<BLEPeripheralInterface, BLEStatus> bleStatus = new Dictionary<BLEPeripheralInterface, BLEStatus>();
+
 
     void Start()
     {
@@ -32,18 +38,25 @@ public class Sample_ConnectName : MonoBehaviour
 
         var addrsToRemove = this.cubeItems.Keys.ToList();
 
-        // Display connected items
-        foreach (var peripheral in this.connectedPeripherals) {
+        // Display items
+        foreach (var peripheral in this.bleStatus.Keys) {
             var item = TryGetCubeItem(peripheral);
             item.transform.SetSiblingIndex(idx++);
-            addrsToRemove.Remove(peripheral.device_address);
-        }
 
-        // Display scanned items
-        foreach (var peripheral in this.scannedPeripherals) {
-            if (peripheral == null) continue;
-            var item = TryGetCubeItem(peripheral);
-            item.transform.SetSiblingIndex(idx++);
+            var status = this.bleStatus[peripheral];
+            if (status == BLEStatus.Disconnected) {
+                item.GetComponentInChildren<Button>().interactable = true;
+                item.GetComponentInChildren<Text>().text = peripheral.device_name + ": Disconnected";
+            }
+            else if (status == BLEStatus.Connecting) {
+                item.GetComponentInChildren<Button>().interactable = false;
+                item.GetComponentInChildren<Text>().text = peripheral.device_name + ": Connecting...";
+            }
+            else if (status == BLEStatus.Connected) {
+                item.GetComponentInChildren<Button>().interactable = true;
+                item.GetComponentInChildren<Text>().text = peripheral.device_name + ": Connected";
+            }
+
             addrsToRemove.Remove(peripheral.device_address);
         }
 
@@ -62,7 +75,7 @@ public class Sample_ConnectName : MonoBehaviour
         {
             var item = Instantiate(this.cubeItemPrefab, this.listContent);
             item.GetComponent<Button>().onClick.AddListener(async () => await OnItemClick(item, peripheral));
-            item.GetComponentInChildren<Text>().text = peripheral.device_name + (peripheral.isConnected? ": connected" : ": paired");
+            item.GetComponentInChildren<Text>().text = peripheral.device_name + (peripheral.isConnected? ": connected" : ": disconnected");
             this.cubeItems.Add(peripheral.device_address, item);
             return item;
         }
@@ -74,8 +87,6 @@ public class Sample_ConnectName : MonoBehaviour
         // Clear list (except connected items)
         foreach (var addr in this.cubeItems.Keys.ToArray())
         {
-            if (this.connectedAddrs.Contains(addr))
-                continue;
             Destroy(this.cubeItems[addr]);
             this.cubeItems.Remove(addr);
         }
@@ -94,7 +105,18 @@ public class Sample_ConnectName : MonoBehaviour
 
     void OnScanUpdate(BLEPeripheralInterface[] peripherals)
     {
-        this.scannedPeripherals = peripherals.ToList();
+        foreach (var peri in peripherals) {
+            if (peri == null) continue;
+            if (this.bleStatus.ContainsKey(peri) && this.bleStatus[peri] != BLEStatus.Connecting)
+                this.bleStatus[peri] = BLEStatus.Disconnected;
+            if (!this.bleStatus.ContainsKey(peri))
+                this.bleStatus.Add(peri, BLEStatus.Disconnected);
+        }
+        foreach (var peri in this.bleStatus.Keys.ToArray()) {
+            if (!peripherals.Contains(peri) && this.bleStatus[peri] == BLEStatus.Disconnected) {
+                this.bleStatus.Remove(peri);
+            }
+        }
 
         // Add connection listener
         foreach (var peripheral in peripherals)
@@ -110,53 +132,32 @@ public class Sample_ConnectName : MonoBehaviour
 #if !(UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN)
             // NOTE: On Windows, disconnecting a device causes crash
             this.connecter.Disconnect(peripheral);
-            await UniTask.Delay(200);
-            if (item)
-                item.GetComponentInChildren<Text>().text = peripheral.device_name + ": paired";
+            this.bleStatus[peripheral] = BLEStatus.Disconnected;
 #endif
         }
         else{
-            item.GetComponentInChildren<Button>().interactable = false;
-            item.GetComponentInChildren<Text>().text = peripheral.device_name + ": connecting...";
+            this.bleStatus[peripheral] = BLEStatus.Connecting;
             try {
                 var cube = await this.connecter.Connect(peripheral);
-                if (cube == null) {
-                    item.GetComponentInChildren<Text>().text = peripheral.device_name + ": failed";
-                }
+                if (cube == null)
+                    throw new System.Exception("Connection Timeout.");
             }
             catch (System.Exception e) {
-                if (item)
-                    item.GetComponentInChildren<Text>().text = peripheral.device_name + ": failed";
 #if UNITY_IOS || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
                 // Connectition fail twice causes BLE host device lost issue on iOS/macOS
-                if (this.connectedPeripherals.Contains(peripheral))
-                    this.connectedPeripherals.Remove(peripheral);
+                this.bleStatus.Remove(peripheral);
+#else
+                this.bleStatus[peripheral] = BLEStatus.Disconnected;
 #endif
                 Debug.LogError(e);
-                return;
             }
-            if (item)
-                item.GetComponentInChildren<Button>().interactable = true;
         }
     }
 
     void OnConnection(BLEPeripheralInterface peripheral)
     {
-        if (peripheral.isConnected)
-        {
-            if (!this.connectedPeripherals.Contains(peripheral))
-                this.connectedPeripherals.Add(peripheral);
-
-            if (this.cubeItems.ContainsKey(peripheral.device_address))
-                this.cubeItems[peripheral.device_address].GetComponentInChildren<Text>().text = peripheral.device_name + ": connected";
-        }
-        else
-        {
-            if (this.connectedPeripherals.Contains(peripheral))
-                this.connectedPeripherals.Remove(peripheral);
-
-            if (this.cubeItems.ContainsKey(peripheral.device_address))
-                this.cubeItems[peripheral.device_address].GetComponentInChildren<Text>().text = peripheral.device_name + ": paired";
-        }
+        this.bleStatus[peripheral] = peripheral.isConnected ? BLEStatus.Connected : BLEStatus.Disconnected;
+        if (this.cubeItems.ContainsKey(peripheral.device_address))
+            this.cubeItems[peripheral.device_address].GetComponentInChildren<Text>().text = peripheral.device_name + (peripheral.isConnected? ": connected" : ": disconnected");
     }
 }
