43658cee43745a38f0dfa6481619bb9a14329730
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2024-04-22 21:10:06 +0900

    Update Sample_DigitalTwin.cs

--- toio-sdk-unity/Assets/toio-sdk/Samples/Sample_DigitalTwin/Sample_DigitalTwin.cs
+++ toio-sdk-unity/Assets/toio-sdk/Samples/Sample_DigitalTwin/Sample_DigitalTwin.cs
@@ -9,11 +9,26 @@ public class Sample_DigitalTwin : MonoBehaviour
 {
     public DigitalTwinConnector connector;
 
+    Dictionary<string, CubeHandle> handles = new Dictionary<string, CubeHandle>();
+    float elapsedTime = 0;
+
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime < 0.05f) return;
+        elapsedTime = 0;
+
         foreach (var cube in this.connector.cubes)
         {
-            // Write your codes here
+            Debug.Log(cube.localName);
+            // Create CubeHandle for new cube
+            if (!this.handles.ContainsKey(cube.addr))
+                this.handles.Add(cube.addr, new CubeHandle(cube));
+
+            // Control cube with CubeHandle
+            var handle = this.handles[cube.addr];
+            handle.Update();
+            handle.Move2Target(255, 255).Exec();
         }
     }
 
