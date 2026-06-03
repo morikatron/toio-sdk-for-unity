cb3c8d32a1339d46144f3fa62cfd1e0ef1bff546
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2026-06-03 18:26:35 +0900

    Update CubeConnecter.cs

--- toio-sdk-unity/Assets/toio-sdk/Scripts/Cube/CubeConnecter.cs
+++ toio-sdk-unity/Assets/toio-sdk/Scripts/Cube/CubeConnecter.cs
@@ -12,8 +12,6 @@ namespace toio
         UniTask<Cube[]> Connect(BLEPeripheralInterface[] peripherals);
         void Disconnect(Cube cube);
         void Disconnect(BLEPeripheralInterface peripheral);
-        [Obsolete("Deprecated. Please use ReConnect(Cube cube) instead.", false)]
-        UniTask ReConnect(Cube cube, BLEPeripheralInterface peripheral);
         UniTask ReConnect(Cube cube);
     }
 
@@ -28,9 +26,9 @@ namespace toio
         {
             if (ConnectType.Auto == type)
             {
-#if (UNITY_EDITOR || UNITY_STANDALONE)
+#if (UNITY_EDITOR)
                 this.impl = new SimImpl();
-#elif (UNITY_IOS || UNITY_ANDROID || UNITY_WEBGL)
+#else
                 this.impl = new RealImpl();
 #endif
             }
